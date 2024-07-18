fe29b5e64d6a63066623b9579f3741e4b5cae2c4
indusree783 <indusree783@gmail.com>
2024-07-18 20:40:57 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,18 @@
+package FirstProject;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TO display 10 sentences about myself
+				System.out.println("Iam Indusree.");		
+				System.out.println("I am native to tirupathi.");
+				System.out.println("Currently i am pursuing MCA.");
+				System.out.println("I am a good listener.");
+				System.out.println("I belong to a nuclear family.");
+				System.out.println("Iam a nature lover.");
+				System.out.println("My hobbies are listening music and watching movies.");
+				System.out.println("My short term and long term goal is to get better job in MNC companies.");
+				System.out.println("I like watching rom-com and action films");
+				System.out.println("I enjoy learning new things.");
+	}
+}
