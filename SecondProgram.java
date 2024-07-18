7166eb446960326845d37e6fa6ec4a6433c09a1c
indusree783 <indusree783@gmail.com>
2024-07-18 20:42:24 +0530

    Add files via upload

---
+++ SecondProgram.java
@@ -0,0 +1,19 @@
+package FirstProject;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Computers are powerful machines that process information.");		
+		System.out.println("They consist of hardware and software components.");
+		System.out.println("The central processing unit(CPU) is the brain of the computer.");
+		System.out.println("They store data on hard drives or solid state drives.");
+		System.out.println("Computers connect to the internet through wifi.");
+		System.out.println("They enable global communications.");
+		System.out.println("They have processors, memory and storage.");
+		System.out.println("Computers can be used for gaming,video editing,and software development.");
+		System.out.println("They run operating systems like windows,macOS,or linux. ");
+		System.out.println("They play a vital role in various industries including education,etc");
+
+	}
+
+}
