f162b4bcfa3c43387e2f2dc0c3ab330f19fd3069
indusree783 <indusree783@gmail.com>
2024-07-18 20:44:22 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,12 @@
+package FirstProject;
+//java program to calculate simple interest.
+public class SimpleInterest {
+	public static void main(String[] args)
+	{
+		int p=13000; //p=principal amount
+		int r=10;    //r=rate
+		int t=2;     //t=time
+		double SimpleInterest=p*r*t/100;
+		System.out.println(SimpleInterest);
+	}
+}
